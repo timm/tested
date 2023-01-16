2f44b743292f1802ff558ff7d223d38f787418f4
Tim Menzies <timm@ieee.org>
2023-01-16 01:13:54 -0500

    save

--- src/fetchr.lua
+++ src/fetchr.lua
@@ -8,12 +8,14 @@ USAGE: lua fetchr.lua [OPTIONS] [-g ACTION]
 OPTIONS:
   -b  --budget     max peeking budget           = 20
   -B  --Bins       how to divide data           = 16
+  -c  --contrasts  number of contrasts          = 512
+  -C  --Conf       confidence for stats         = 95
+  -d  --dull       cliff's delta threshold      = .147
   -f  --file       csv file to load             = ../etc/data/auto93.csv
   -F  --Far        how far is long distances    = .95
   -g  --go         start up action              = nothing
   -h  --help       show help                    = false
   -m  --min        min size of clusters         = .5
-  -c  --contrasts  number of contrasts          = 512
   -p  --p          distance coefficient         = 2
   -s  --seed       random number seed           = 937162211 
   -S  --Sample     search space for clustering  = 512]]
@@ -28,8 +30,8 @@ local function O(s,    t) --> t; create a klass and a constructor
   return setmetatable(t, {__call=function(_,...) 
     local i=setmetatable({a=s},t); return setmetatable(t.new(i,...) or i,t) end}) end
 
-local any,cells,cli,coerce,copy,csv,fmt,kap,keys,lines,lt
-local main,many,map,o,oo,push,settings,rand,rint,rnd,sort,Seed
+local any,cells,cli,cliffsDelta,coerce,copy,critical,csv,fmt,kap,keys,lines,lt
+local main,many,map,mwu,o,oo,push,settings,rand,rank,ranks,rint,rnd,sort,Seed
 --------------------------------------------------------------------------------------
 --[[
 About this code:
@@ -69,7 +71,7 @@ In the function arguments, the following conventions apply (usually):
 
 In my object system, instances are named `i` (since that is shorter than `self`).--]]
 --------------------------------------------------------------------------
-local SYM,NUM,DATA,ROW,BIN=O"SYM",O"NUM",O"DATA",O"ROW",O"BIN" -- classes
+local SYM,NUM,DATA,ROW,XY=O"SYM",O"NUM",O"DATA",O"ROW",O"XY" -- classes
 local COL,COLS -- factories
 --------------------------------------------------------------------------------------
 function COL(n,s,    col)
@@ -102,9 +104,9 @@ function SYM:add(s,  inc)
 
 function SYM:mid() return self.mode end
 function SYM:div(      e,fun)
-   fun = function(p) return p*math.log(p,2) end
-   e=0; for _,n in pairs(self.seen) do e = e + fun(n/self.n) end;
-   return -e end
+  fun = function(p) return p*math.log(p,2) end
+  e=0; for _,n in pairs(self.seen) do e = e + fun(n/self.n) end;
+  return -e end
 
 function SYM:bucket(s) return s end
 function SYM:tekcub(s) return s end
@@ -147,12 +149,6 @@ function NUM:dist(n1,n2)
   if n1=="?" then n1= n2<.5 and 1 or 0 end
   if n2=="?" then n2= n1<.5 and 1 or 0 end
   return math.abs(n1-n2) end
-
--- function force(col,x,inc)
---   if x =="?" then return end
---   if col.isNum then
---     gap=(col.hi - col.lo)/the.Bins
---
 --------------------------------------------------------------------------
 function ROW:new(t) return {cells=t,yused=false} end
 
@@ -217,21 +213,30 @@ function DATA:half(  rows,cols,above)
     push(n <= (#rows//2) and left or right, tmp.row) end
   return left, right, A, B, c end  
 
-function DATA:sway() --> t; returns best half, recursively
-  local other,recurse,halve={}
-  function recurse(yes,no,above)
-    for _,row in pairs(no) do push(other,row) end
-    return halve(yes,above) end
-  function halve(rows,  above)
-    if #rows < (#self.rows)^the.min then return rows,other else 
-      local left, right, A, B = self:half(rows,self.cols.x,above)
-      return self:better(B,A) and recurse(right,left,B) or recurse(left,right,A) end end
-  return halve(self.rows) end
+function DATA:sway(  rows,other,above) --> t; returns best half, recursively
+  rows  = rows or self.rows
+  if   #rows < 2*(#self.rows)^the.min 
+  then return rows,other 
+  else local as, bs, a, b = self:half(rows,self.cols.x,above)
+       if self:better(b,a) then as,bs,a,b = bs,as,b,a end
+       other = other or bs
+       return self:sway(as,other,a) end end 
 
 function DATA:stats(  what,cols,nPlaces) --> t; reports mid or div of cols (defaults to i.cols.y)
   local fun
   function fun(k,col) return rnd(getmetatable(col)[what or "mid"](col),nPlaces),col.txt end
   return kap(cols or self.cols.all, fun) end
+
+function DATA:diffs(data,  cols,     diff)
+  function diff(col,    x) 
+    function x(row,     z)  z = row.cells[col.at]; if z~="?" then return z end end
+    local x1s,x2s = map(self.rows,x), map(data.rows,x) 
+    local n1,n2   = #x1s, #x2s
+    local m = math.min(n1,n2)
+    x1s = many(x1s,m)
+    x2s = many(x2s,m)
+    return mwu(x1s,x2s) and not cliffsDelta(x1s,x2s),col.txt end
+  return map(cols or self.cols.y, diff) end
 
 -------------------------------------------------------------------------
 function XY:new(lo,hi) return{xlo=lo,xhi=lo or hi or lo,ys={},n=0} end
@@ -270,6 +275,89 @@ function XY.merges(xys,    fun) -- {hi,lo,yes,no,n,     all,merge1}
     return #xys0 == #xys1 and xys1 or fun(xys1) 
   end -----------------------------
   return fun(sort(xys,lt"lo")) end
+-------------------------------------------------------------------------
+function cliffsDelta(ns1,ns2, dull) --> bool; true if different by a trivial amount
+  local n,gt,lt = 0,0,0
+  for _,x in pairs(ns1) do
+    for _,y in pairs(ns2) do
+      n = n + 1
+      if x > y then gt = gt + 1 end
+      if x < y then lt = lt + 1 end end end
+  return math.abs(lt - gt)/n <= (dull or the.dull) end
+---------------------------------------------------------------------------------------------------
+function rank(rx) return rx.ranks/rx.n end --> n; returns average range in a treatment  
+
+function mwu(ns1,ns2,nConf) -->bool; True if ranks of `ns1,ns2` are different at confidence `nConf` 
+  local t,r1,r2,u1,u2,c = ranks(ns1,ns2)
+  local n1,n2= #ns1, #ns2
+  assert(n1>=3,"must be 3 or more")
+  assert(n2>=3,"must be 3 or more")
+  c  = critical(nConf or the.Conf or 95,n1,n2)
+  r1=0; for _,x in pairs(ns1) do r1=r1+ rank(t[x]) end
+  r2=0; for _,x in pairs(ns2) do r2=r2+ rank(t[x]) end
+  u1 = n1*n2 + n1*(n1+1)/2 - r1
+  u2 = n1*n2 + n2*(n2+1)/2 - r2
+  local word = math.min(u1,u2)<=c 
+  return math.min(u1,u2)<=c  end  -- not evidence evidence to say they are the same
+
+function critical(c,n1,n2)
+  local t={
+    [99]={{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2,  2,  2,  3,  3},
+          {0, 0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 5, 5,  6,  6,  7,  8},
+          {0, 0, 0, 1, 1, 2, 3, 4, 5, 6, 7, 7, 8, 9, 10, 11, 12, 13},
+          {0, 0, 1, 2, 3, 4, 5, 6, 7, 9,10,11,12,13, 15, 16, 17, 18},
+          {0, 0, 1, 3, 4, 6, 7, 9,10,12,13,15,16,18, 19, 21, 22, 24},
+          {0, 1, 2, 4, 6, 7, 9,11,13,15,17,18,20,22, 24, 26, 28, 30},
+          {0, 1, 3, 5, 7, 9,11,13,16,18,20,22,24,27, 29, 31, 33, 36},
+          {0, 2, 4, 6, 9,11,13,16,18,21,24,26,29,31, 34, 37, 39, 42},
+          {0, 2, 5, 7,10,13,16,18,21,24,27,30,33,36, 39, 42, 45, 48},
+          {1, 3, 6, 9,12,15,18,21,24,27,31,34,37,41, 44, 47, 51, 54},
+          {1, 3, 7,10,13,17,20,24,27,31,34,38,42,45, 49, 53, 56, 60},
+          {1, 4, 7,11,15,18,22,26,30,34,38,42,46,50, 54, 58, 63, 67},
+          {2, 5, 8,12,16,20,24,29,33,37,42,46,51,55, 60, 64, 69, 73},
+          {2, 5, 9,13,18,22,27,31,36,41,45,50,55,60, 65, 70, 74, 79},
+          {2, 6,10,15,19,24,29,34,39,44,49,54,60,65, 70, 75, 81, 86},
+          {2, 6,11,16,21,26,31,37,42,47,53,58,64,70, 75, 81, 87, 92},
+          {3, 7,12,17,22,28,33,39,45,51,56,63,69,74, 81, 87, 93, 99},
+          {3, 8,13,18,24,30,36,42,48,54,60,67,73,79, 86, 92, 99,105}},
+    [95]={{0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6,  6,  7,  7,  8},
+          {0, 0, 1, 2, 3, 4, 4, 5, 6, 7, 8, 9,10,11, 11, 12, 13, 14},
+          {0, 1, 2, 3, 5, 6, 7, 8, 9,11,12,13,14,15, 17, 18, 19, 20},
+          {1, 2, 3, 5, 6, 8,10,11,13,14,16,17,19,21, 22, 24, 25, 27},
+          {1, 3, 5, 6, 8,10,12,14,16,18,20,22,24,26, 28, 30, 32, 34},
+          {2, 4, 6, 8,10,13,15,17,19,22,24,26,29,31, 34, 36, 38, 41},
+          {2, 4, 7,10,12,15,17,20,23,26,28,31,34,37, 39, 42, 45, 48},
+          {3, 5, 8,11,14,17,20,23,26,29,33,36,39,42, 45, 48, 52, 55},
+          {3, 6, 9,13,16,19,23,26,30,33,37,40,44,47, 51, 55, 58, 62},
+          {4, 7,11,14,18,22,26,29,33,37,41,45,49,53, 57, 61, 65, 69},
+          {4, 8,12,16,20,24,28,33,37,41,45,50,54,59, 63, 67, 72, 76},
+          {5, 9,13,17,22,26,31,36,40,45,50,55,59,64, 67, 74, 78, 83},
+          {5,10,14,19,24,29,34,39,44,49,54,59,64,70, 75, 80, 85, 90},
+          {6,11,15,21,26,31,37,42,47,53,59,64,70,75, 81, 86, 92, 98},
+          {6,11,17,22,28,34,39,45,51,57,63,67,75,81, 87, 93, 99,105},
+          {7,12,18,24,30,36,42,48,55,61,67,74,80,86, 93, 99,106,112},
+          {7,13,19,25,32,38,45,52,58,65,72,78,85,92, 99,106,113,119},
+          {8,14,20,27,34,41,48,55,62,69,76,83,90,98,105,112,119,127}}}
+    n1,n2 = n1-2,n2-2
+    local u=t[c]
+    assert(u,"confidence level unknown")
+    local n1 = math.min(n1,#u[1])
+    local n2 = math.min(n2,#u)
+    return u[n2][n1] end
+
+function ranks(ns1,ns2) -->t; numbers of both populations are jointly ranked 
+  local x,t,u = 0,{},{}
+  for _,ns in pairs{ns1,ns2} do
+    for _,x in pairs(ns) do t[1+#t] = x end end
+  t = sort(t)
+  x = t[1]
+  u[x] = {x=x,n=1,ranks=1}
+  for i=2,#t do
+    if t[i-1] ~= t[i] then x=t[i]; u[x] = {x=x, n=0,ranks=0}  end    
+    u[x].x     = t[i]
+    u[x].ranks = u[x].ranks + i -- for repeated numbers, they share the rank of all the repeats
+    u[x].n     = u[x].n + 1 end
+  return u end
 -------------------------------------------------------------------------
 fmt = string.format
 function any(t) --> any; return any item from `t`, picked at random
@@ -353,8 +441,8 @@ function main(funs,settings,txt,    fails,saved)
   fails,saved = 0,copy(settings)
   if   settings.help 
   then print(txt)
-       print("\nACTIONS:\n  -g",". (runs all actions)") 
-       for _,name in pairs(keys(funs)) do print("  -g",name) end
+       print("\nACTIONS:\n  -g  .  (runs all actions)") 
+       for _,name in pairs(keys(funs)) do print("  -g   "..name) end
   else for _,name in pairs(keys(funs)) do
         if name:find(".*"..settings.go..".*") then
           for k,v in pairs(saved) do settings[k]=v end
@@ -366,8 +454,7 @@ function main(funs,settings,txt,    fails,saved)
 
 -------------------------------------------------------------------------
 local egs={}
-function egs.show_config() oo(the) end
-function egs.test_maths()  print(10 + 10) end
+function egs.alternatives() oo(the) end
 function egs.NUM_test()    oo(COL(2,"Asda+")) end
 
 function egs.cols_test(     t) 
@@ -398,14 +485,14 @@ function egs.dists(    data)
     print(j,fmt("%s",data:dist(data.rows[1],data.rows[j]),
                      o(data.rows[j].cells))) end end
 
-function egs.betters(     data,t)
+function egs.row_sort(     data,t)
   data=DATA(the.file)
   t=sort(data.rows, function(a,b) return data:better(a,b) end)
   oo(data.cols.names)
   for j= 1,#data.rows,50 do
     print(j, o(t[j].cells)) end end
 
-function egs.half(    data)
+function egs.row_split(    data)
   data=DATA(the.file)
   local left,right,A,B,c = data:half() 
   left = data:clone(left)
@@ -413,7 +500,27 @@ function egs.half(    data)
   print("left",o(left:stats("mid",left.cols.y)))
   print("right",o(right:stats("mid",right.cols.y))) end
 
--------------------------------------------------------------------------
+function egs.row_sway(    data,a,b)
+  data=DATA(the.file)
+  a,b = data:sway() 
+  a = data:clone(a) 
+  b = data:clone(b) 
+  oo(a:diffs(b, a.cols.y))
+  print("a", o(a:stats("mid",a.cols.y)),
+             o(a:stats("div",a.cols.y)))
+  print("b", o(b:stats("mid",b.cols.y)),
+             o(b:stats("div",b.cols.y)))
+end
+
+function egs.stats_eg1()
+  print("false",mwu( {8,7,6,2,5,8,7,3},{8,7,6,2,5,8,7,3}))
+  print("true",mwu( {8,7,6,2,5,8,7,3}, {9,9,7,8,10,9,6})) end
+
+function egs.stats_eg2()
+  print("false",mwu( {8,7,6,2,5,8,7,3},{8,7,6,2,5,8,7,3}))
+  print("true",mwu( {8,7,6,2,5,8,7,3}, {9,9,7,8,10,9,6})) end
+
+ -------------------------------------------------------------------------
 settings(help, the)
 
 if pcall(debug.getlocal,4,1) 
